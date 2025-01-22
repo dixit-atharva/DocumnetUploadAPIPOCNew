2114afa5412a207e35a2a9013ed700eff6ac5fdf
Rahul Patel <rahul.patel@atharvasystem.com>
2025-01-22 15:53:37 +0530

    licence key added

--- Syncfusion.WordEditor/syncfusion.wordeditor.client/src/main.ts
+++ Syncfusion.WordEditor/syncfusion.wordeditor.client/src/main.ts
@@ -2,6 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import { registerLicense } from '@syncfusion/ej2-base';
+
+// Registering Syncfusion license key
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NMaF5cXmBCf0x0THxbf1x1ZFFMZF1bRnRPMyBoS35Rc0ViW31edHBURGJeWEx+');
 
 platformBrowserDynamic().bootstrapModule(AppModule)
 .catch((err : any) =>
